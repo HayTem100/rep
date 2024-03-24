daeb70228b3958dfb7d69ba0905bb12ffdcd7aff
Haytem <132968624+HayTem100@users.noreply.github.com>
2024-03-24 09:30:20 +0000

    Delete emailslic.py

--- emailslic.py
+++
@@ -1,9 +0,0 @@
-#-------------------------
-#-----Email Slicing-------
-#-------------------------
-email = input("Welcome... enter your email : ").strip().lower()
-user_name = email[:email.index("@")]
-email_part = email[email.index("@")+1:]
-
-print(f"####[User name] ==> [{user_name}]######")
-print(f"####[emailDomain] ==> [{email_part}]#####")
